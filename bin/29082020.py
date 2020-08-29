43b18dadc013d952e8a658f62e31742f51c252c9
Leonardo <ea2982@gmail.com>
2020-08-29 15:57:23 +0300

    edit 29082020.py

--- bin/29082020.py
+++ bin/29082020.py
@@ -1,24 +1,21 @@
 '''Functions'''
 
+
 def fn(*arg):
     print(type(arg))
     print(arg)
+
+
 fn()
 fn(1, 'a', None, False)
 
-def greet(*arg):
-    print(len(arg))
-    if len(arg) == 0:
-        return None
-    lst = list(arg)
-    print(len(lst))
 
-    if len(lst) == 0:
-        return
-    hi = 'Hello, ' +  ' and '.join(lst)
+def greet(name, *arg):
+    lst = list((name,) + arg)
+    hi = 'Hello, ' + ' and '.join(lst)
     return hi
+
 
 print(greet('Tom', 'Ann'))
 names = ['Tom', 'Ann']
 greet()
-
