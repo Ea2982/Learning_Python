6aeb2a73868857774cf81be25f9bb12e81fe8493
Leonardo <ea2982@gmail.com>
2020-08-31 17:24:48 +0300

    edit 29082020.py

--- bin/temp.py
+++ bin/temp.py
@@ -1,13 +1,21 @@
+def filter_map(func, items):
+    # lst = map(make_stars, items)
+    # print(lst)
+    lst = list()
+
+    for i1, i2 in map(make_stars, items):
+        if i1:
+            lst.append(i2)
+    return lst
 
 
->>> def make_stars(x):
-...     if x > 0:
-...         return True, '*' * x
-...     return False, ''
-...
->>> for s in filter_map(make_stars, [1, 0, 5, -5, 2]):
-...     print(s)
-...
-*
-*****
-**
+
+def make_stars(x):
+    if x > 0:
+        return True, '*' * x
+    return False, ''
+
+for s in filter_map(make_stars, [1, 0, 5, -5, 2]):
+    print(s)
+
+
