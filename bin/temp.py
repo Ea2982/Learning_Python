e3e0cba440051f1b74913f5305151328803e06ed
Leonardo <ea2982@gmail.com>
2020-08-31 14:41:19 +0300

    edit 29082020.py

--- bin/temp.py
+++ bin/temp.py
@@ -1,7 +1,19 @@
+def push_and_count(target, *, value):
+    print(target)
+    target.append(target)
+    return len(target)
+
 def shoot():
     return 'Bang!'
-def call_twice(fn, **args):
-    return fn(), fn()
+
+def call_twice(func, *args, **kwargs):
+
 
 #print(call_twice(input, 'Enter value'))
-print(call_twice(shoot))
+#call_twice(shoot)
+
+
+
+
+print(call_twice(push_and_count, [], value=42))
+
