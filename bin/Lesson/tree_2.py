1d441531bc0d3ba74aa2d478fa01150f0f13a0b7
Leonardo <ea2982@gmail.com>
2020-09-04 14:17:38 +0300

    edit tree_2.py

--- bin/Lesson/tree_2.py
+++ bin/Lesson/tree_2.py
@@ -27,7 +27,46 @@ def mkdir(name, children=[], meta={}):
 # print(tree)
 
 def generator():
-    tree = mkdir('python-package', [
+    tree = mkdir(
+        'python-package',
+        meta={'hidden': True},
+        children=[
+            mkfile('Makefile'),
+            mkfile('README.md'),
+            mkdir('dist'),
+            mkdir('tests', [
+                mkfile('test_solution.py')
+            ]),
+            mkfile('myproject.toml'),
+            mkdir(
+                '.venv',
+                meta={'owner': 'root', 'hidden': False},
+                children= [
+                    mkdir(
+                        'lib',
+                        [
+                            mkdir(
+                                'python3.6',
+                                [
+                                    mkdir(
+                                        'site-packages',
+                                        [
+                                            mkfile('hexlet-python-package.egg-link')
+                                        ]
+                                    )
+                                ]
+                            )
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
+
+    return tree
+print(generator())
+
+'''tree = mkdir('python-package', [
         mkfile('Makefile'),
         mkfile('README.md'),
         mkdir('dist'),
@@ -49,4 +88,37 @@ def generator():
     ], {'hidden': True})
     return tree
 
-print(generator())
+
+
+
+{'name': 'python-package', 
+ 'children': [
+     {'name': 'Makefile', 'meta': {}, 'type': 'file'},
+     {'name': 'README.md', 'meta': {}, 'type': 'file'}, 
+     {'name': 'dist', 'children': [], 'meta': {}, 'type': 'directory'}, 
+     {
+         'name': 'tests',
+         'children': 
+             [{'name': 'test_solution.py', 'meta': {}, 'type': 'file'}], 
+         'meta': {}, 
+         'type': 'directory'
+     }, 
+     {'name': 'pyproject.toml', 'meta': {}, 'type': 'file'}, 
+     {
+         'name': '.venv', 
+         'children': [{
+             'name': 'lib', 
+             'children': [{
+                 'name': 'python3.6', 
+                 'children': [{
+                     'name': 'site-packages', 
+                     'children': [{
+                         'name': 'hexlet-python-package.egg-link', 
+                         'meta': {}, 'type': 'file'}], 
+                     'meta': {}, 'type': 'directory'}], 
+                 'meta': {}, 'type': 'directory'}], 
+             'meta': {}, 'type': 'directory'}], 
+         'meta': {'owner': 'root', 'hidden': False}, 'type': 'directory'}], 
+ 'meta': {'hidden': True}, 'type': 'directory'}
+    
+'''
