6f45ea4ddb2fe91aaf83cd9d90a0cd9a9cbaada4
Leonardo <ea2982@gmail.com>
2020-09-04 13:42:08 +0300

    edit tree_2.py

--- bin/Lesson/tree_2.py
+++ bin/Lesson/tree_2.py
@@ -15,3 +15,38 @@ def mkdir(name, children=[], meta={}):
             'meta': meta,
             'type': 'directory',
            }
+
+# tree = mkdir('etc',[
+#         mkdir('bashrc'),
+#         mkdir('consul', [
+#             mkfile('config.json')
+#               ])
+#     ]
+# )
+#
+# print(tree)
+
+def generator():
+    tree = mkdir('python-package', [
+        mkfile('Makefile'),
+        mkfile('README.md'),
+        mkdir('dist'),
+        mkdir('tests', [
+            mkfile('test_solution.py')
+        ]),
+        mkfile('pyproject.toml'),
+        mkdir('.venv', [
+            mkdir('lib', [
+                mkdir('python3.6', [
+                    mkdir('site-packages', [
+                        mkfile('hexlet-python-package.egg-link')
+                    ])
+                ])
+            ])
+        ], {'owner': 'root', 'hidden': False})
+
+
+    ], {'hidden': True})
+    return tree
+
+print(generator())
